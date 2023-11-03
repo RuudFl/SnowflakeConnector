fcdcc90cd9a5c6a3a8a859e25c03799e2507df16
RuudFl <rudolphus@gmail.com>
2023-11-03 10:40:31 +0100

    updated to snowflake jdbc 3.13.34

--- src/javasource/databaseconnector/interfaces/ConnectionManager.java
+++
@@ -1,8 +0,0 @@
-package databaseconnector.interfaces;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionManager {
-  Connection getConnection(final String jdbcUrl, final String userName, final String password) throws SQLException;
-}
--- src/javasource/databaseconnector/interfaces/ObjectInstantiator.java
+++
@@ -1,8 +0,0 @@
-package databaseconnector.interfaces;
-
-import com.mendix.systemwideinterfaces.core.IContext;
-import com.mendix.systemwideinterfaces.core.IMendixObject;
-
-public interface ObjectInstantiator {
-  IMendixObject instantiate(final IContext context, final String entityName);
-}
